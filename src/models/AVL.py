0670a7a872b7c95efdc703870b737a922f05520e
julianparra26 <julian.parra@ucaldas.edu.co>
2026-09-20 18:36:03 -0500

    Probando que funciona(No edite nada)

--- src/models/AVL.py
+++ src/models/AVL.py
@@ -16,8 +16,7 @@ class AVL(Tree):
     def getBalanceFactor(self):
         return self.__balanceFactor
 
-    def setBalanceFactor(self, balanceFactor):
-        self.__balanceFactor = balanceFactor
+    
 
     def __str__(self):
         return (
